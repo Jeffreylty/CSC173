feb6ad4d752ea6b61459e5a63ba5dd18ef63c289
Canruo <ruby.hongbaoshi@163.com>
2018-11-17 23:10:00 -0500

    Update Proj.c

--- Proj.c
+++ Proj.c
@@ -60,3 +60,46 @@ CSGHASHTABLE* insert_CSG(char* Courses, int StudentId, char* Grade, CSGHASHTABLE
     }
     
 }
+
+int main(void) {
+	//part 1.3
+	//populate the tables with the data given in the figures
+	insert_CSG(new_CSG("CS101", 12345, "A"), Course-StudentID-Grade);
+	insert_CSG(new_CSG("CS101", 67890, "B"), Course-StudentID-Grade);
+	insert_CSG(new_CSG("EE200", 12345, "C"), Course-StudentID-Grade);
+	insert_CSG(new_CSG("EE200", 22222, "B+"), Course-StudentID-Grade);
+	insert_CSG(new_CSG("CS101", 33333, "A-"), Course-StudentID-Grade);
+	insert_CSG(new_CSG("PH100", 67890, "A+"), Course-StudentID-Grade);
+
+	insert_SNAP(new_SNAP(12345, "C. Brown", "12 Apple St.", "555-1234"), StudentID-Name-Address-Phone);
+	insert_SNAP(new_SNAP(67890, "L. Van Pelt", "34 Pear Ave.", "555-5678"), StudentID-Name-Address-Phone);
+	insert_SNAP(new_SNAP(22222, "P. Patty", "56 Grape Blvd.", "555-9999"), StudentID-Name-Address-Phone);
+
+	insert_CP(new_CP("CS101", "CS100"), Course-Prerequisite);
+	insert_CP(new_CP("EE200", "EE005"), Course-Prerequisite);
+	insert_CP(new_CP("EE200", "CS100"), Course-Prerequisite);
+	insert_CP(new_CP("CS120", "CS101"), Course-Prerequisite);
+	insert_CP(new_CP("CS121", "CS120"), Course-Prerequisite);
+	insert_CP(new_CP("CS205", "CS101"), Course-Prerequisite);
+	insert_CP(new_CP("CS206", "CS121"), Course-Prerequisite);
+	insert_CP(new_CP("CS206", "CS205"), Course-Prerequisite);
+
+	insert_CDH(new_CDH("CS101", "M", "9AM"), Course-Day-Hour);
+	insert_CDH(new_CDH("CS101", "W", "9AM"), Course-Day-Hour);
+	insert_CDH(new_CDH("CS101", "F", "9AM"), Course-Day-Hour);
+	insert_CDH(new_CDH("EE200", "Tu", "10AM"), Course-Day-Hour);
+	insert_CDH(new_CDH("EE200", "W", "1PM"), Course-Day-Hour);
+	insert_CDH(new_CDH("EE200", "Th", "10AM"), Course-Day-Hour);
+
+	insert_CR(new_CR("CS101", "Turing Aud."), Course-Room);
+	insert_CR(new_CR("EE200", "25 Ohm Hall"), Course-Room);
+	insert_CR(new_CR("PH100", "Newton Lab."), Course-Room);
+
+	printf("Find the grade of the student with ID 12345 in CS101: %s\n", print_CSG(lookup((“CS101”, 12345, ∗), Course-StudentId-Grade)));
+	printf("Whether CS120 is a prerequisite of CS205: %s\n", print_CP(lookup((“CS205”, "CS120"), Course-Prerequisite)));
+	printf("Drop the first tuple from CR: %s\n", print_CR(delete((“CS101”, ∗), Course-Room)));
+	printf("Makes CS120 a prerequisite of CS205: %s\n", print_CP(insert((“CS205”, "CS120"), Course-Prerequisite)));
+	printf("Has no effect on the relation of CP, because the inserted tuple is already there: %s\n", print_CP(insert((“CS205”, "CS101"), Course-Prerequisite)));
+
+	return 0;
+}
